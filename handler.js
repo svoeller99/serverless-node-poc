b72aeec0de764f5a39c103a0345a91dfd1f51af0
Voeller, Sean <Sean.Voeller@nike.com>
2017-03-28 13:37:57 -0700

    refactor / split functions

--- handler.js
+++ handler.js
@@ -3,116 +3,115 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
-module.exports.hello = (event, context, callback) => {
-  const method = event.requestContext.httpMethod;
-  const pathParams = event.pathParameters;
-  const body = event.body;
+function respond(callback, statusCode, body) {
+  const response = { statusCode };
+  if (body) {
+    response.body = JSON.stringify(body);
+  }
+  callback(null, response);
+}
 
-  if (method == 'POST' || method == 'PUT') {
-    const user = JSON.parse(body);
-    if (method == 'PUT') {
-      user.email = pathParams.email;
+function notFound(callback) {
+  respond(callback, 404);
+}
+
+function ok(body, callback) {
+  respond(callback, 200, body);
+}
+
+function created(body, callback) {
+  respond(callback, 201, body);
+}
+
+function internalServerError(error, callback) {
+  console.error(error);
+  respond(callback, 500, error);
+}
+
+module.exports.get = (event, context, callback) => {
+  const email = event.pathParameters.email;
+  const params = {
+    TableName: 'usersTable',
+    Key: {
+      email: email
     }
-    const params = {
-      TableName: 'usersTable',
-      Item: user
-    };
-
-    return dynamo.put(params, (err, result) => {
-      if (err) {
-        callback(null, {
-          statusCode: 500,
-          body: JSON.stringify(err)
-        });
-        return;
-      }
-
-      callback(null, {
-        statusCode: method == 'POST' ? 201 : 200,
-        body: JSON.stringify(user)
-      });
-    });
-  }
-
-  if (method == 'GET') {
-    const email = pathParams.email;
-    const params = {
-      TableName: 'usersTable',
-      Key: {
-        email: email
-      }
-    };
-
-    return dynamo.get(params, (err, result) => {
-      try {
-        if (err) {
-          callback(null, {
-            statusCode: 500,
-            body: JSON.stringify(err)
-          });
-          return;
-        }
-
-        if (!result.Item) {
-          callback(null, {
-            statusCode: 404
-          });
-        }
-
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(result.Item)
-        });
-      } catch (e) {
-        callback(e, null);
-      }
-    });
-  }
-
-  if (method == 'DELETE') {
-    const email = pathParams.email;
-    const params = {
-      TableName: 'usersTable',
-      Key: {
-        email: email
-      }
-    };
-
-    return dynamo.delete(params, (err, result) => {
-      try {
-        if (err) {
-          callback(null, {
-            statusCode: 500,
-            body: JSON.stringify(err)
-          });
-          return;
-        }
-
-        callback(null, {
-          statusCode: 200
-        });
-      } catch (e) {
-        callback(e, null);
-      }
-    });
-  }
-
-/*
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully! Your method was ' + method,
-      input: event,
-    })
   };
 
-  callback(null, response);
-*/
+  return dynamo.get(params, (err, result) => {
+    try {
+      if (err) {
+        return internalServerError(err, callback);
+      }
 
-  callback(null, {
-    statusCode: 405
+      if (!result.Item) {
+        return notFound(callback);
+      }
+
+      ok(result.Item, callback);
+    } catch (e) {
+      internalServerError(e, callback);
+    }
   });
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.post = (event, context, callback) => {
+  const user = JSON.parse(event.body);
+  const params = {
+    TableName: 'usersTable',
+    Item: user
+  };
+
+  return dynamo.put(params, (err, result) => {
+    try {
+      if (err) {
+        return internalServerError(err, callback);
+      }
+
+      created(user, callback);
+    } catch (e) {
+      internalServerError(e, callback);
+    }
+  });
+};
+
+module.exports.put = (event, context, callback) => {
+  const user = JSON.parse(event.body);
+  user.email = event.pathParameters.email;
+  const params = {
+    TableName: 'usersTable',
+    Item: user
+  };
+
+  return dynamo.put(params, (err, result) => {
+    try {
+      if (err) {
+        return internalServerError(err, callback);
+      }
+
+      ok(user, callback);
+    } catch (e) {
+      internalServerError(e, callback);
+    }
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: 'usersTable',
+    Key: {
+      email: event.pathParameters.email
+    }
+  };
+
+  return dynamo.delete(params, (err, result) => {
+    try {
+      if (err) {
+        return internalServerError(err, callback);
+      }
+
+      ok(null, callback);
+    } catch (e) {
+      internalServerError(e, callback);
+    }
+  });
 };
